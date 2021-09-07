1646275addff6fca0b07c7332aab6b3675b6c016
Libardo <libardojesusrengifo129@gmail.com>
2021-09-07 14:55:09 -0400

    cors

--- server/config/variables.js
+++ server/config/variables.js
@@ -9,13 +9,17 @@ const SERVER = {
   PORT: env.API_PORT || 6000,
   DEV: env.DEV || false,
   API: {
-    COOKIE_EXPIRE_DAYS : 2,
+    COOKIE_EXPIRE_DAYS: 2,
     SALT_BCRYPT: env.SALT_BCRYPT,
     SECRET_TOKEN: env.SECRET_TOKEN,
     SECRET_TOKEN_SESSION: env.SECRET_TOKEN_SESSION,
     SECRET_TOKEN_COOKIE: env.SECRET_TOKEN_COOKIE,
     IS_PRODUCTION: env.NODE_ENV === "production",
-    ALLOWED_DOMAINS: ["http://127.0.0.1:3000", "http://localhost:3000"],
+    ALLOWED_DOMAINS: [
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+      "https://mern-template-ruddy.vercel.app",
+    ],
     RATE_LIMITS: {
       windowMs: 10 * 60 * 1000, // 10 minutes
       max: 200, // limit each IP to 200 requests per windowMs
